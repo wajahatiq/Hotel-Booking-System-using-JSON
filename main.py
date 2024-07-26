0d46f1800249b3092f6759ce248c912fda2b341f
wajahatiq <wajahatv@gmail.com>
2024-07-25 18:48:57 -0700

    3rd commit, creating add books method

--- main.py
+++ main.py
@@ -11,3 +11,16 @@ def load_bookings():
 def save_bookings(bookings):
     with open(BOOKINGS_FILE, 'w') as file:
         json.dump(bookings, file, indent=4)
+
+# Add booking method
+def add_booking(guest_name, room_number, check_in_date, check_out_date):
+    bookings = load_bookings()
+    new_booking = {
+        'guest_name': guest_name,
+        'room_number': room_number,
+        'check_in_date': check_in_date,
+        'check_out_date': check_out_date
+    }
+    bookings.append(new_booking)
+    save_bookings(bookings)
+    print(f"Booking added for {guest_name} in room {room_number}.")
