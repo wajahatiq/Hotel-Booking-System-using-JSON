f9256dd62e23b1756e212cf58e486cb9f5964f9b
wajahatiq <wajahatv@gmail.com>
2024-07-25 18:30:49 -0700

    2nd commit, saving data to json file

--- main.py
+++ main.py
@@ -7,4 +7,7 @@ def load_bookings():
     with open(BOOKINGS_FILE, 'r') as file:
         return json.load(file)
 
-
+# Saving data to JSON
+def save_bookings(bookings):
+    with open(BOOKINGS_FILE, 'w') as file:
+        json.dump(bookings, file, indent=4)
